86f6f8557b9e876d9a7c2d1eac83290e83d50bdd
Piotr Mucha <p.mucha@edrone.me>
2019-05-09 14:18:35 +0200

    added ads

--- app/src/main/java/com/muszkin/bmianalyzer/BmiCalculator.java
+++ app/src/main/java/com/muszkin/bmianalyzer/BmiCalculator.java
@@ -3,10 +3,10 @@ package com.muszkin.bmianalyzer;
 public class BmiCalculator {
 
     public static float calculateBmi(float height,float weight) {
-        return ((float)Math.round((weight/Math.pow(height,2)))*100)/100;
+        return (float)Math.round((weight/Math.pow(height,2))*100) /100;
     }
 
     public static float calcualteWeight(float height,float bmi) {
-        return ((float)Math.round(bmi * Math.pow(height,2))*100)/100;
+        return (float)Math.round((bmi * Math.pow(height,2))*100) /100;
     }
 }
